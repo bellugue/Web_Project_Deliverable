0178276ae19db11418f5866781e6c1ddd3533157
arnaumarti <arnaums02@gmail.com>
2024-03-17 21:23:17 +0100

    Some changes in settings.py

--- First_Deliverable/settings.py
+++ First_Deliverable/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Car_Renting.apps.CarRentingConfig',
+    'Car_Renting',
 ]
 
 MIDDLEWARE = [
