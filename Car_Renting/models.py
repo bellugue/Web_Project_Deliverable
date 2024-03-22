65a9385203cf79ad1678df3435c8c079aeac78e0
arnaumarti <arnaums02@gmail.com>
2024-03-22 12:36:43 +0100

    new class in models

--- Car_Renting/models.py
+++ Car_Renting/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 # Create your models here.
+class CarListView(models.Model):
+    def __str__(self):
+        return self
+
 class Car(models.Model):
     name = models.CharField(max_length=100)
 
