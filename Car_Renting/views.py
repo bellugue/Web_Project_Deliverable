1824526e8a7b5fcf33a67a0a9d0d544f39303fc0
arnaumarti <arnaums02@gmail.com>
2024-03-26 12:46:04 +0100

    some changes

--- Car_Renting/views.py
+++ Car_Renting/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from Car_Renting.models import Car
+
 
 # Create your views here.
+
+class CarRentingView(ListView):
+    model = Car
+    template_name =
