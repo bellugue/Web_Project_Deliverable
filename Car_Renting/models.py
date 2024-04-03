bf033f1d45258a913e05c20e09fb1b3f37b0d519
quimsb03 <quimsanchezbenet@gmail.com>
2024-04-03 19:13:13 +0200

    Models.py fixed, added foreignkeys

--- Car_Renting/models.py
+++ Car_Renting/models.py
@@ -11,7 +11,17 @@ class Business(models.Model):
     def __str__(self):
         return self.NIF
 
+class AuthorisedDealer(models.Model):
+    NIF_bussines = models.ForeignKey(Business, on_delete=models.CASCADE, related_name='NIF_bussines')
+    id_authorisedDealer = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    schedule = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.id_authorisedDealer
+
 class Car(models.Model):
+    AuthorisedDealer = models.ForeignKey(AuthorisedDealer, on_delete=models.CASCADE, related_name="AuthorisedDealer")
     name = models.CharField(max_length=100)
     licensePlate = models.CharField(max_length=100, null=True)
     model = models.CharField(max_length=100, null=True)
@@ -21,17 +31,10 @@ class Car(models.Model):
     def __str__(self):
         return self.name
 
-class AuthorisedDealer(models.Model):
-    id_authorisedDealer = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    schedule = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.id_authorisedDealer
 
 class Rent(models.Model):
     NIF = models.ForeignKey(Business, on_delete=models.CASCADE)
-    licensePlate = models.ForeignKey(Car, on_delete=models.CASCADE)
+    car_rented = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='car_rented')
     id_authorisedDealer = models.ForeignKey(AuthorisedDealer, on_delete=models.CASCADE)
     availability = models.CharField(max_length=100)
     price = models.IntegerField()
