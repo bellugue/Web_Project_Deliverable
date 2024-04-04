78d3ccb02942741f33313e6d7529cb90e39ba8db
Joelsf10 <joelsam388@gmail.com>
2024-04-04 19:35:47 +0200

    Retoques a CarList i CarSelection

--- Car_Renting/models.py
+++ Car_Renting/models.py
@@ -11,17 +11,7 @@ class Business(models.Model):
     def __str__(self):
         return self.NIF
 
-class AuthorisedDealer(models.Model):
-    NIF_bussines = models.ForeignKey(Business, on_delete=models.CASCADE, related_name='NIF_bussines')
-    id_authorisedDealer = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    schedule = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.id_authorisedDealer
-
 class Car(models.Model):
-    AuthorisedDealer = models.ForeignKey(AuthorisedDealer, on_delete=models.CASCADE, related_name="AuthorisedDealer")
     name = models.CharField(max_length=100)
     licensePlate = models.CharField(max_length=100, null=True)
     model = models.CharField(max_length=100, null=True)
@@ -31,16 +21,20 @@ class Car(models.Model):
     def __str__(self):
         return self.name
 
+class AuthorisedDealer(models.Model):
+    id_authorisedDealer = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    schedule = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.id_authorisedDealer
 
 class Rent(models.Model):
     NIF = models.ForeignKey(Business, on_delete=models.CASCADE)
-    car_rented = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='car_rented')
+    licensePlate = models.ForeignKey(Car, on_delete=models.CASCADE)
     id_authorisedDealer = models.ForeignKey(AuthorisedDealer, on_delete=models.CASCADE)
-    fecha_entrada = models.DateField()
-    fecha_salida = models.DateField()
-    nombre_cliente = models.CharField(max_length=100)
-    telefono_cliente = models.CharField(max_length=20)
-    correo_cliente = models.EmailField(default=None)
+    availability = models.CharField(max_length=100)
+    price = models.IntegerField()
 
-def __str__(self):
-    return f'{str(self.id_authorisedDealer)} @{str(self.NIF)} - Car: {str(self.car_rented)}'
+    def __str__(self):
+        return f'{str(self.id_authorisedDealer) + "@" + str(self.NIF) + "@" + str(self.licensePlate)}'
